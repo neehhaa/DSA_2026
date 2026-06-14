3e32064df6e685f90e6b0016249f86ae18b3f940
neehhaa <nehawasnik95@outlook.com>
2026-06-14 17:59:29 +0100

    pattern 2

--- Patterns/pattern2.py
+++ Patterns/pattern2.py
@@ -8,7 +8,7 @@
 
 lines = int(input("Enter number of lines: "))
 
-for outer in range (lines):
-    for inner in range (outer+1):
+for outer in range (0, lines):
+    for inner in range (0, outer+1):
         print("*", end="")
     print()
